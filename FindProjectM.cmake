edaceeceeefeed6a9ec1097fef4db8b228a08c4f
wsnipex <wsnipex@a1.net>
2014-09-02 18:37:43 +0200

    use pkg-config to detect GLEW and freetype and use shared projectm

--- FindProjectM.cmake
+++ FindProjectM.cmake
@@ -11,12 +11,12 @@ if(PKG_CONFIG_FOUND)
   list(APPEND PROJECTM_INCLUDE_DIRS ${PROJECTM_INCLUDEDIR})
 endif()
 
-if(NOT PROJECTM_FOUND)
-  find_path(PROJECTM_INCLUDE_DIRS libprojectM/projectM.hpp)
-  find_library(PROJECTM_LIBRARIES projectM)
-endif()
+#if(NOT PROJECTM_FOUND)
+#  find_path(PROJECTM_INCLUDE_DIRS libprojectM/projectM.hpp)
+#  find_library(PROJECTM_LIBRARIES projectM)
+#endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(ProjectM DEFAULT_MSG PROJECTM_INCLUDE_DIRS PROJECTM_LIBRARIES)
+find_package_handle_standard_args(ProjectM DEFAULT_MSG PROJECTM_INCLUDE_DIRS PROJECTM_LIBRARIES PROJECTM_LIBRARY_DIRS)
 
 mark_as_advanced(PROJECTM_INCLUDE_DIRS PROJECTM_LIBRARIES)
