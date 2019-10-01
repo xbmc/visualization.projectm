c54fc15027e3466f6832446949397ff36f81d768
Alwin Esch <alwin.esch@web.de>
2019-10-01 10:25:01 +0200

    update cmake find GL & GLES way

--- FindOpenGLES.cmake
+++ FindOpenGLES.cmake
@@ -18,7 +18,7 @@ if(PKG_CONFIG_FOUND)
   pkg_check_modules(PC_OPENGLES ${_brcmprefix}glesv2 QUIET)
 endif()
 
-if(NOT CORE_SYSTEM_NAME STREQUAL ios)
+if(NOT CORE_SYSTEM_NAME STREQUAL darwin_embedded AND NOT CORE_SYSTEM_NAME STREQUAL ios)
   find_path(OPENGLES_INCLUDE_DIR GLES2/gl2.h
                                  PATHS ${PC_OPENGLES_INCLUDEDIR})
   find_library(OPENGLES_gl_LIBRARY NAMES ${_brcmprefix}GLESv2
@@ -31,13 +31,24 @@ else()
   set(OPENGLES_INCLUDE_DIR ${OPENGLES_gl_LIBRARY}/Headers)
 endif()
 
+find_path(OPENGLES3_INCLUDE_DIR GLES3/gl3.h)
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(OpenGLES
                                   REQUIRED_VARS OPENGLES_gl_LIBRARY OPENGLES_INCLUDE_DIR)
 
+find_path(OPENGLES3_INCLUDE_DIR GLES3/gl3.h
+                                PATHS ${PC_OPENGLES_INCLUDEDIR})
+
 if(OPENGLES_FOUND)
   set(OPENGLES_LIBRARIES ${OPENGLES_gl_LIBRARY})
-  set(OPENGLES_INCLUDE_DIRS ${OPENGLES_INCLUDE_DIR})
-  set(OPENGLES_DEFINITIONS -DHAS_GLES=2)
-  mark_as_advanced(OPENGLES_INCLUDE_DIR OPENGLES_gl_LIBRARY)
+  if(OPENGLES3_INCLUDE_DIR)
+    set(OPENGLES_INCLUDE_DIRS ${OPENGLES_INCLUDE_DIR} ${OPENGLES3_INCLUDE_DIR})
+    set(OPENGLES_DEFINITIONS -DHAS_GLES=3)
+    mark_as_advanced(OPENGLES_INCLUDE_DIR OPENGLES3_INCLUDE_DIR OPENGLES_gl_LIBRARY)
+  else()
+    set(OPENGLES_INCLUDE_DIRS ${OPENGLES_INCLUDE_DIR})
+    set(OPENGLES_DEFINITIONS -DHAS_GLES=2)
+    mark_as_advanced(OPENGLES_INCLUDE_DIR OPENGLES_gl_LIBRARY)
+  endif()
 endif()
